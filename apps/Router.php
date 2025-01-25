8eab0dea175deffac71d1405dc88a1b3091d6800
ahmadsopyan9 <ahmadsofian63@gmail.com>
2025-01-25 14:42:01 +0700

    update

--- apps/Router.php
+++ apps/Router.php
@@ -1,9 +1,9 @@
 <?php 
 defined('_ROOT_') OR exit('No direct script access allowed');
 
+// test
+// $route['blog/(:any)'] = 'blog/detail/$1';
 
 // Route
 $route['default'] = 'home';
-$route['blog/(:any)'] = 'blog/detail/$1';
-
 $route['logout'] = 'auth/logout';
