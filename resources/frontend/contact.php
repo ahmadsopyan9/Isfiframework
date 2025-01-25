61767f8937afafb8a65bd5b5f48617e8c5911466
ahmadsopyan9 <ahmadsofian63@gmail.com>
2025-01-25 14:40:55 +0700

    Update Resources View

--- resources/frontend/contact.php
+++ resources/frontend/contact.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="<?=asset('frontend/css/style.css');?>">
+    <link rel="icon" href="<?=favicon();?>" type="image/png">
+    <script src="<?=asset('plugins/jquery/dist/jquery.min.js');?>"></script>
+    <script src="<?=asset('plugins/alert/isAlert.min.js');?>"></script>
 </head>
-<body>
+<body id="mp" data-scr="<?=crsf_token();?>">
     <div class="container">
         <h1><?= $title ?></h1>
         <div class="content">
